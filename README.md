647f6aac4860f3b99579c27f606cae3e3233eb5a
Thomas Abplanalp <info@tasoft.ch>
2020-01-22 12:19:01 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# php-dependency-injection
-PHP Dependency Injection library in collaboration with service manager
+# Skyline CLI Package
+The CLI Package adds a plugin to your application that interact on command line requests.  
+By default, the file ```~/Public/skyline.php``` gets called by apache or another webserver to deliver your webpage.
+
+If this file gets performed under command line, you will get an error, because the routing never will success.  
+This Package installed it will listen for command line calls and forward to registered tasks.
+
+#### Installation
+```bin
+$ composer require skyline/cli
+```
+
+#### Usage
+The package introduces a new configuration file:  
+``processes.cfg.php``  
+All files with this name are collected while compiling and they should declare a process info.
+
+Such a config file might look like:
+```php
+<?php
+// File MyClass.php
+class MyClass {
+    // Constructor must not expect arguments.
+    // If you need so, specify as service and define with ProcessConfig::PROCESS_SERVICE_NAME
+    public function __construct() {}
+    
+    public function run($argc, $argv) {
+        // Do stuff
+    }
+}
+
+// File: processes.cfg.php
+
+use Skyline\CLI\Config\ProcessConfig;
+
+return [
+    [
+        ProcessConfig::PROCESS_NAME => 'my-process',
+        ProcessConfig::PROCESS_METHOD => 'run',
+        ProcessConfig::PROCESS_CLASS_NAME => stdClass::class
+    ]
+];
+```
+
+
---
+++ src/Plugin/SkylineCLIPlugin.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2020, TASoft Applications
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace Skyline\CLI\Plugin;
+
+
+use TASoft\EventManager\EventManager;
+
+class SkylineCLIPlugin
+{
+    public function checkCLICommand(string $eventName, $event, EventManager $eventManager, ...$arguments)
+    {
+        if(php_sapi_name() == 'cli') {
+            global $argv;
+            array_shift($argv); // Binary file
+            $cmd = array_shift($argv); // first argument which must be the process
+
+
+
+
+            $eventManager->trigger(SKY_EVENT_TEAR_DOWN);
+            exit();
+        }
+    }
+}
